66fb82c0853efad29cfbb94c78f8c6dbf8c534cf
Jamel <jamelg310@gmail.com>
2015-06-16 11:29:19 -0700

    change in database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,55 +11,53 @@
 #
 
 FactoryGirl.define do
-factory :user do
+  factory :user do
 
 
-##GENERAL
+  ##GENERAL
 
-  factory :valid_user, class: User do
+    factory :valid_user, class: User do
 
-    email "brodievegas@gmail.com"
-    password "vegas123"
-    password_confirmation "vegas123"
+      email "brodievegas@gmail.com"
+      password "vegas123"
+      password_confirmation "vegas123"
+
+    end
+
+
+    factory :null_email, class: User do
+
+      email nil
+      password "password"
+      password_confirmation "password"
+
+
+    end
+
+    factory :long_email, class: User do
+
+      email " getyourgameupbrodie@gmail.com"
+      password "password"
+      password_confirmation "password"
+
+    end
+
+    factory :long_password, class: User do
+
+      email " getyourgameupbrodie@gmail.com"
+      password "password fjklassdfjlk"
+      password_confirmation "password fjkladjkls"
+
+    end
+
+    factory :dif_password, class: User do
+
+      email " getyourgameupbrodie@gmail.com"
+      password "password"
+      password_confirmation "passwordlkjlkji"
+
+    end
 
   end
-
-
-  factory :null_email, class: User do
-
-    email nil
-    password "password"
-    password_confirmation "password"
-
-
-  end
-
-  factory :long_email, class: User do
-
-    email " getyourgameupbrodie@gmail.com"
-    password "password"
-    password_confirmation "password"
-
-  end
-
-  factory :long_password, class: User do
-
-    email " getyourgameupbrodie@gmail.com"
-    password "password fjklassdfjlk"
-    password_confirmation "password fjkladjkls"
-
-  end
-
-  factory :dif_password, class: User do
-
-    email " getyourgameupbrodie@gmail.com"
-    password "password"
-    password_confirmation "passwordlkjlkji"
-
-  end
-
-
-
-end
 end
 
